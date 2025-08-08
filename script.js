0384b8bb434d8bd8cdd6a5dffa2a05611525eb14
Doub1eW89 <pro100invest@bk.ru>
2025-08-08 11:42:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ function renderProducts(products) {
     const grid = document.querySelector('.products-grid');
     grid.innerHTML = products.map(product => `
         <div class="product-card" data-category="${product.category}">
+            <div class="product-image">
+                ${product.images.map(img => `
+                    <img src="assets/photos/${img}" alt="${product.name}">
+                `).join('')}
+            </div>
             <h3>${product.name}</h3>
             <p>${product.description}</p>
             <p>Готовый: ${product.priceReady} ₽</p>
